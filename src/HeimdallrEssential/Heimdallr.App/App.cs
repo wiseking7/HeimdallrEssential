1b799ffab29034f9a1b56fa72cd4e1d9034d2898
wiseking7 <69136610+wiseking7@users.noreply.github.com>
2025-04-30 12:41:08 +0900

    App.xaml, MainWindow.xaml 삭제

---
+++ src/HeimdallrEssential/Heimdallr.App/App.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace Heimdallr.App;
+
+internal class App : Application
+{
+  protected override void OnStartup(StartupEventArgs e)
+  {
+    base.OnStartup(e);
+
+    Window win = new Window();
+    win.Title = "HEIMDALLR";
+
+    // Loaded 이벤트 핸들러
+    win.Loaded += (s, e) =>
+    {
+      Debug.WriteLine($"Width: {win.ActualWidth}, Height: {win.ActualHeight}");
+    };
+
+    win.Show();
+  }
+
+
+}
---
+++ src/HeimdallrEssential/Heimdallr.App/Starter.cs
@@ -0,0 +1,11 @@
+﻿namespace Heimdallr.App;
+
+class Starter
+{
+  [STAThread]
+  private static void Main(string[] args)
+  {
+    App app = new App();
+    app.Run();
+  }
+}
